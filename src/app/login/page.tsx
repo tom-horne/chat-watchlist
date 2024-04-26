4417c9b51b6e52fe66d2d3f8b1df8204fe386ae9
Tom Horne <tom@madamot.com>
2024-04-26 15:50:16 +0100

    feat: - added basic navbar - created component for login/signup form - error when authenticated users try access login/signup pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,20 @@
+import AuthBox from "@/components/authbox/authbox";
 import { login } from "./actions";
 import Link from "next/link";
+import { createClient } from "../../../utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const supabase = createClient();
+  const { data, error } = await supabase.auth.getUser();
+  if (data?.user) {
+    redirect("/error");
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <Link href="/">
-        <button className="bg-green-200 p-4">Home</button>
-      </Link>
       <>
-        <form className="bg-blue-800 p-4 flex flex-col items-center justify-between">
-          <label htmlFor="email">Email:</label>
-          <input id="email" name="email" type="email" required />
-          <label htmlFor="password">Password:</label>
-          <input id="password" name="password" type="password" required />
-          <button className="bg- bg-orange-200 p-2" formAction={login}>
-            Login
-          </button>
-        </form>
+        <AuthBox submit={login} title="Login" />
       </>
       <Link href="/signup">
         <p>New around? Click here to signup</p>
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,61 @@
+import Link from "next/link";
+import { createClient } from "../../../utils/supabase/server";
+import { signout } from "@/app/logout/actions";
+
+type NavBarProps = {};
+
+const NavBar: React.FC<NavBarProps> = async ({}) => {
+  const supabase = createClient();
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    return (
+      <header className="w-full border-0 border-b-[0.5px] border-solid border-slate-300 shadow-inner top-0">
+        <nav className="flex justify-between items-center h-16 max-w-5xl mx-auto">
+          <div className="mr-auto">
+            <Link href="/">
+              <h3>Title</h3>
+            </Link>
+          </div>
+
+          <div>
+            <ul className="flex gap-x-6">
+              <Link href="/login">
+                <li>Login</li>
+              </Link>
+              <Link href="/signup">
+                <li>Signup</li>
+              </Link>
+              <li></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+  return (
+    <header className="w-full border-0 border-b-[0.5px] border-solid border-slate-300 shadow-inner top-0">
+      <nav className="flex justify-between items-center h-16 max-w-5xl mx-auto">
+        <div className="mr-auto">
+          <Link href="/">
+            <h3>Title</h3>
+          </Link>
+        </div>
+
+        <div>
+          <ul className="flex gap-x-6">
+            <li>
+              <p>Hello, {data.user.email}</p>
+            </li>
+            <li>
+              <form>
+                <button formAction={signout}>Signout</button>
+              </form>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
