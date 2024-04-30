654604545a110c9b7973906880a3421a320c6e34
Tom Horne <tom@madamot.com>
2024-04-30 22:44:52 +0100

    changes to supabase actions to work with react hook forms

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -4,14 +4,19 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { createClient } from '../../../utils/supabase/server'
 
+type FormData = {
+  email: string
+  password: string
+}
+
 export async function login(formData: FormData) {
   const supabase = createClient()
 
   // type-casting here for convenience
   // in practice, you should validate your inputs
   const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
+    email: formData.email,
+    password: formData.password,
   }
 
   const { error } = await supabase.auth.signInWithPassword(data)
