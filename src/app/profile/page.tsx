3d2044f297217e730ff64d02c2804cfe38b5a9e6
Tom Horne <tom@madamot.com>
2024-05-01 23:34:12 +0100

    feat: - show extra user data from userinfo table - need to create form to add userinfo through profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,30 +2,65 @@ import React from "react";
 import { createClient } from "../../../utils/supabase/server";
 import { redirect } from "next/navigation";
 
-type ProfileProps = {};
+type ProfileProps = {
+  username: string;
+  firstname: string;
+  lastname: string;
+};
 
 const Profile: React.FC<ProfileProps> = async () => {
   const supabase = createClient();
   const { data, error } = await supabase.auth.getUser();
+
   if (error || !data?.user) {
-    redirect("/");
+    redirect("/error");
   }
+
+  const { data: UserInfo } = await supabase
+    .from("UserInfo")
+    .select()
+    .eq("id", data.user.id);
+
+  //   if (!UserInfo) {
+  //     <div>No User data</div>;
+  //   }
+
+  const Username = UserInfo && UserInfo[0]?.username;
+  const Firstname = UserInfo && UserInfo[0]?.firstname;
+  const Lastname = UserInfo && UserInfo[0]?.lastname;
+
   return (
     <main className="flex flex-col items-center">
       <div className="flex flex-col p-4 gap-3">
-        <label htmlFor="email">Email</label>
-        <p className="border" id="email">
-          {data.user.email}
+        <p>{data.user.email}</p>
+        <p>{Username}</p>
+        <p>
+          {Firstname} {Lastname}
         </p>
-        <form className="flex flex-col gap-3">
+        {/* <form className="flex flex-col gap-3">
           <label htmlFor="username">Username</label>
-          <input className="border" id="username" type="username" />
+          <input
+            className="border"
+            id="username"
+            type="username"
+            value={Username}
+          />
           <label htmlFor="firstname">First Name</label>
-          <input className="border" id="firstname" type="firstname" />
-          <label htmlFor="surname">Surname</label>
-          <input className="border" id="surname" type="surname" />
+          <input
+            className="border"
+            id="firstname"
+            type="firstname"
+            value={Firstname}
+          />
+          <label htmlFor="lastname">Last name</label>
+          <input
+            className="border"
+            id="lastname"
+            type="lastname"
+            value={Lastname}
+          />
           <button className="bg-blue-600 p-2 rounded">Save Details</button>
-        </form>
+        </form> */}
       </div>
     </main>
   );
